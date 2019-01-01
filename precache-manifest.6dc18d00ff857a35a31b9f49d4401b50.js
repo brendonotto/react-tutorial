e5a49ef72e8593bd42e50df828586d893804bb39
Brendon Otto <brendon@brendonotto.com>
2018-12-31 23:54:58 -0600

    Updates

---
+++ precache-manifest.6dc18d00ff857a35a31b9f49d4401b50.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "058c239b5120d343a2a0",
+    "url": "/react-tutorial/static/css/main.c82bfd7d.chunk.css"
+  },
+  {
+    "revision": "058c239b5120d343a2a0",
+    "url": "/react-tutorial/static/js/main.058c239b.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/react-tutorial/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "1372e2a4ee1d05bb6722abbb4837fcde",
+    "url": "/react-tutorial/index.html"
+  }
+];
